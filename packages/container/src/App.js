7387ab8a25b947019d6452dc8a36d9376e4ac83b
Haofu <haofu.zhu@gmail.com>
2021-06-04 21:27:34 +1000

    add container header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </div>
+    </BrowserRouter>
   )
 }
 
